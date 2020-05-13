7da30a54bf3e3f838e9db59315fae2c013170073
YangJ0605 <1442122744@qq.com>
2020-05-13 17:21:23 +0800

    删除一些无用文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import './App.scss';
 import styled from 'styled-components'
 
-const Button = styled.button({
-  color:'grey',
-  background:'white'
-})
-const Button2 = styled.button`
-  color: red;
-  background:green;
-  &:hover {
-    background:red;
-  }
-  border: 10px solid red;
-`
+
 
 function App() {
   
   return (
-    <div>
-      <Button>你好</Button>
-      <Button2>你好2</Button2>
-    </div>
+   <div>hello</div>
   );
 }
 
