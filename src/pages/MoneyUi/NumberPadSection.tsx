ba31df7c7ee0c233e9679a7793d4239fea6ec587
YangJ0605 <1442122744@qq.com>
2020-05-19 00:18:58 +0800

    重构键盘部分逻辑

--- src/pages/MoneyUi/NumberPadSection.tsx
+++ src/pages/MoneyUi/NumberPadSection.tsx
@@ -1,70 +1,14 @@
 import React, { FC, useState, MouseEvent } from 'react'
-import styled from 'styled-components'
-
-
-
-const Wrapper = styled.section`
-display:flex;
-flex-direction:column;
-.output {
-  background:white;
-  font-size:36px;
-  line-height:72px;
-  text-align:right;
-  padding:0 16px;
-  box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,.25),
-              inset 0  5px 5px -5px rgba(0,0,0,.25);
-}
-.pad {
-  /* display:flex; */
-  /* flex-wrap:wrap; */
-  button {
-    width:25%;
-    height:64px;
-    float:left;
-    border:none;
-    &.ok {
-      height:128px;
-      float:right;
-    }
-    &.zero {
-      width:50%
-    }
-    &.point {font-size:34px}
-    &:nth-child(2),
-    &:nth-child(5) {
-        background:#E0E0E0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background:#D3D3D3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background:#C1C1C1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background:#B8B8B8;
-      }
-      &:nth-child(12) {
-        background:#9A9A9A;
-      }
-      &:nth-child(14) {
-        background:#A9A9A9;
-      }
-      &:active {
-          background:#ccc;
-        }
-  }
-}
-`
+import Wrapper from './NumberPadSection/Wrapper'
 
 const NumberPadSection: FC = () => {
-  const [output, setOutput] = useState<string>('0')
+  const [output, _setOutput] = useState<string>('0')
+  const setOutput = (output:string) => {
+    if(output.length > 16) {
+      output = output.slice(0,16)
+    }
+    _setOutput(output)
+  }
   const handleClick = (e: MouseEvent) => {
     const text = (e.target as HTMLElement).textContent
     if (!text) return
@@ -99,11 +43,16 @@ const NumberPadSection: FC = () => {
         setOutput('0')
         break
       case '删除':
-        if(output === '0') return
+        /* if(output === '0') return
         const arr = output.split('')
         if(arr.length === 1) return
         arr.splice(arr.length - 1)
-        setOutput(arr.join(''))
+        setOutput(arr.join('')) */
+        if(output.length === 1) {
+          setOutput('0')
+        } else {
+          setOutput(output.slice(0, -1))
+        }
         break
     }
 
