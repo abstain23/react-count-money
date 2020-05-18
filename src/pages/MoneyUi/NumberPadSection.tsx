0da2921ebca1315248702c47a3dbb72422b86768
YangJ0605 <1442122744@qq.com>
2020-05-18 18:11:31 +0800

    完成部分NumberPadSection功能

--- src/pages/MoneyUi/NumberPadSection.tsx
+++ src/pages/MoneyUi/NumberPadSection.tsx
@@ -1,8 +1,9 @@
+import React, { FC, useState, MouseEvent } from 'react'
 import styled from 'styled-components'
 
 
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
 display:flex;
 flex-direction:column;
 .output {
@@ -62,4 +63,71 @@ flex-direction:column;
 }
 `
 
+const NumberPadSection: FC = () => {
+  const [output, setOutput] = useState<string>('0')
+  const handleClick = (e: MouseEvent) => {
+    const text = (e.target as HTMLElement).textContent
+    if (!text) return
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if(output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break
+      case '.':
+        if(output.indexOf(text) >= 0) {
+          return
+        } else {
+          setOutput(output + text)
+        }
+        break
+      case 'OK':
+        console.log(text)
+        break
+      case '清空':
+        setOutput('0')
+        break
+      case '删除':
+        if(output === '0') return
+        const arr = output.split('')
+        if(arr.length === 1) return
+        arr.splice(arr.length - 1)
+        setOutput(arr.join(''))
+        break
+    }
+
+  }
+  return (
+    <Wrapper>
+      <div className='output'>{output}</div>
+      <div className='pad clearfix' onClick={handleClick}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='ok'>OK</button>
+        <button className='zero'>0</button>
+        <button className='point'>.</button>
+      </div>
+    </Wrapper>
+  )
+}
 export default NumberPadSection
