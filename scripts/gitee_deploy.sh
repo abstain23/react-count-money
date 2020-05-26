49fd2b811ff96131e11a3a342715a4a77461cbb6
7605789+rise_gitee@user.noreply.gitee.com <1442122744@qq.com>
2020-05-26 18:25:40 +0800

    修改script

--- scripts/gitee_deploy.sh
+++ scripts/gitee_deploy.sh
@@ -5,6 +5,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin https://gitee.com/rise_gitee/react-count-money.git &&
+git remote add origin https://gitee.com/rise_gitee/reactCountMoney.git &&
 git push -u origin master -f
 cd -
